705d1b6c86274af3bd1d02fb265f1f57572b3560
IB3W <165117417+IB3W@users.noreply.github.com>
2026-01-01 10:15:53 +0200

    Add functions to delete all and done tasks

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function confirmDeleteAll() {
+    todos = [];
+    saveToLocalStorage();
+    closeModal(deleteAllModal);
+    render();
+    showMessage('All tasks have been deleted.', 'success');
+}
 
+function deleteDoneTasks() {
+    const doneTasks = todos.filter(todo => todo.done);
+    if (doneTasks.length === 0) {
+        showMessage('No done tasks to delete.', 'info');
+        return;
+    }
+    openModal(deleteDoneModal);
+}
